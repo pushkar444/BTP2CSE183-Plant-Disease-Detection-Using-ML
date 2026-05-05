1bb5d15eb8493d1489734acc9d350e80e29628ea
pushkar kumar <pushkar.kumar.cs28@iilm.edu>
2026-05-06 04:23:30 +0530

    Delete start.bat

--- start.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-call "C:\Users\Abhinav Mishra\anaconda3\Scripts\activate.bat" base
-cd "C:\Users\Abhinav Mishra\OneDrive\Desktop\plant_disease_web"
-start "" "index.html"
-"C:\Users\Abhinav Mishra\anaconda3\python.exe" app.py
-pauses
